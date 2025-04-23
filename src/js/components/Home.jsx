3c62175472b489dc5cd8c958aa43ea5888cb6d2c
Maria Alejandra Robles <alejita-robles@hotmail.com>
2025-04-23 20:04:08 +0000

    finalizado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [activeLight, setActiveLight] = useState (null);
+
+	const light = {
+		redLight: {
+			opacity: activeLight === 'redLight' ? 1 : 0.3,
+			cursor: 'pointer'
+		},
+		yellowLight: {
+			opacity: activeLight === 'yellowLight' ? 1 : 0.3,
+			cursor: 'pointer'
+		},
+		greenLight: {
+			opacity: activeLight === 'greenLight' ? 1 : 0.3,
+			cursor: 'pointer'
+		}
+	}
+
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<h1 className="m-4 ">My traffic Light</h1>
+		<div className="d-flex justify-content-center myContainer "> 
+           <div className="trafficLight">
+			<div className="redLight" style={light.redLight} onClick={() => setActiveLight('redLight')}></div>
+			<div className="yellowLight" style={light.yellowLight} onClick={() => setActiveLight('yellowLight')}></div>
+			<div className="greenLight" style={light.greenLight} onClick={() => setActiveLight('greenLight')}></div>
+		   </div>
+		</div>
 		</div>
 	);
 };
