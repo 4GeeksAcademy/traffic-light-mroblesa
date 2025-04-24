44b24f1dc115f869957eced39b96e9188a1306e8
Maria Alejandra Robles <alejita-robles@hotmail.com>
2025-04-24 20:31:01 +0000

    finalizado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -5,7 +5,6 @@ import React, { useState } from "react";
 const Home = () => {
 
 	const [activeLight, setActiveLight] = useState (null);
-
 	const light = {
 		redLight: {
 			opacity: activeLight === 'redLight' ? 1 : 0.3,
@@ -18,12 +17,18 @@ const Home = () => {
 		greenLight: {
 			opacity: activeLight === 'greenLight' ? 1 : 0.3,
 			cursor: 'pointer'
-		}
-	}
+		} 
+	} 
+//funcion para cambiar color de luz en el boton
+	const changeLight = () =>  {
+		const lights = ['redLight', 'yellowLight', 'greenLight']
+	  const nextIndex = activeLight ? (lights.indexOf(activeLight) + 1) % lights.length: 0;
+    setActiveLight(lights[nextIndex]);
+  };
 
 	return (
 		<div className="text-center">
-		<h1 className="m-4 ">My traffic Light</h1>
+		<h1 className="m-4 ">My Traffic Light</h1>
 		<div className="d-flex justify-content-center myContainer "> 
            <div className="trafficLight">
 			<div className="redLight" style={light.redLight} onClick={() => setActiveLight('redLight')}></div>
@@ -31,6 +36,7 @@ const Home = () => {
 			<div className="greenLight" style={light.greenLight} onClick={() => setActiveLight('greenLight')}></div>
 		   </div>
 		</div>
+		<button className="btn btn-dark m-4 " onClick={changeLight}>Change light</button>
 		</div>
 	);
 };
